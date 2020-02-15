51aa3f87ae27a9ce1ac6e3fe37e62584f7c890ef
RomainGuillaume <romainbertheault@gmail.com>
2020-02-15 01:55:53 +0100

    Rename gameObjects

---
+++ Assets/Scripts/Rift/Rift.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rift : MonoBehaviour
+{
+
+    public GameObject prefabRift;
+    public float interval = .5f;
+
+    void Update()
+    {
+        if (prefabRift != null)
+        {
+            Random.Range(-10.0f, 10.0f);
+        }
+    }
+}
